51555fd59b1a34ff42c55948f40d940735bd7e6b
Ben McMahan <benjamin@linkedresponder.com>
2026-07-13 14:48:11 -0400

    Polish main app UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.pulsepointlabs.polarwiz"
         minSdk = 33
         targetSdk = 35
-        versionCode = 28
-        versionName = "1.0.7"
+        versionCode = 29
+        versionName = "1.0.8"
     }
 
     buildFeatures { viewBinding = true }
